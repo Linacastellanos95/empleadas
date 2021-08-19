f1784b0fe248dc0709fbb0eaf3352bda6a8b42cb
Linacastellanos95 <linamu-ozc@hotmail.com>
2021-08-18 23:08:42 -0500

    Se crea un método GET point (empleados/id)

--- src/main/java/ar/com/ada/api/empleadas/services/EmpleadaService.java
+++ src/main/java/ar/com/ada/api/empleadas/services/EmpleadaService.java
@@ -25,6 +25,14 @@ public class EmpleadaService {
         return repo.findAll();
     }
 
+    public Empleada buscarEmpleada(Integer empleadaId){
+        Optional<Empleada> resultado = repo.findById(empleadaId);
+
+        if(resultado.isPresent())
+            return resultado.get();
+
+        return null;
+    }
 
     
 }
